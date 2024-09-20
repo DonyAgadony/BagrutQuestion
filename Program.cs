fcd43c28e780f1abb28bbf31e94dec631c8f3f84
Daniel <donybof1@gmail.com>
2024-09-20 09:04:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+class TwoStacks(Stack<int> sums, Stack<int> numbers)
+{
+    public Stack<int> sums = sums;
+    public Stack<int> numbers = numbers;
+
+    public Stack<int> GetNums(int x)
+    {
+        Stack<int> rtn = new();
+        int num = 0;
+        do
+        {
+            num = sums.Pop();
+        } while (x != num);
+        int sumR = 0;
+        Stack<int> ReversedNums = new(numbers);
+        while (sumR != x)
+        {
+            sumR += ReversedNums.Pop();
+        }
+        return rtn;
+    }
+
+    public void CalculateSums()
+    {
+        Stack<int> reversedNums = new(numbers);
+        int num = numbers.Pop();
+        sums.Push(num);
+
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
